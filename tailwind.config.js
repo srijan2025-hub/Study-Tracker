baccdf660a25480381d1eda50e5e2e4160054229
srijan2025-hub <srijangithub@gmail.com>
2025-11-30 10:23:20 +0530

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    { pattern: /(bg|text|border)-(indigo|emerald|rose|sky|violet)-(50|100|200|300|400|500|600|700|900)/ }
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
